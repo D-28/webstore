3ea47f40971d5929427cf42233b66f4ec625db3a
Dan Shkolnik <dsmirror28@gmail.com>
2019-04-18 20:56:03 -0400

    stripe checkout

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b764dec62a2d3907258b";
+/******/ 	var hotCurrentHash = "d8687d509ee164ffd086";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
