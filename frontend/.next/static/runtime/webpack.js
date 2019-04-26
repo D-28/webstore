1ee2732cf2cea4c1346608c935741d832f47c60f
Dan Shkolnik <dsmirror28@gmail.com>
2019-04-26 14:20:33 -0400

    prisma datamodel update and syntax changes

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d8687d509ee164ffd086";
+/******/ 	var hotCurrentHash = "a0aee792f134dfe23588";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
